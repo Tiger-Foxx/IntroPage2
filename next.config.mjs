ac43204e2f75d6161e3078de493d23e0c0ccd1d9
Tiger-Foxx <donfackarthur750@gmail.com>
2025-03-20 18:15:58 +0100

    config next redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: "/blog/:id(\\d+)", // Capture uniquement les IDs numériques
+          destination: "https://theoldfox.pythonanywhere.com/post/:id",
+          permanent: false, // Redirection temporaire (true si tu veux une redirection permanente)
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
