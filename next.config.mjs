707c7e4b99977f2215199bbb33a36a3c95c6e735
Tiger-Foxx <donfackarthur750@gmail.com>
2025-05-20 12:13:18 +0100

    redirection corrected

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
       return [
         {
           source: "/blog/:id(\\d+)", // Capture uniquement les IDs numériques
-          destination: "https://theoldfox.pythonanywhere.com/post/:id",
+          destination: "https://site.the-fox.tech/blog/:id",
           permanent: false, // Redirection temporaire (true si tu veux une redirection permanente)
         },
         {
           source: "/projet/:id(\\d+)", // Capture uniquement les IDs numériques
-          destination: "https://theoldfox.pythonanywhere.com/projet/:id",
+          destination: "https://site.the-fox.tech/projects/:id",
           permanent: true, // Redirection temporaire (true si tu veux une redirection permanente)
         },
         {
